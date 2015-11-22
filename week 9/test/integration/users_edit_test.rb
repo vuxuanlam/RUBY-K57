7edf01e8a3f5366e87d04a1bb3d6e18eae4f38b5
vuxuanlam <lamchinsu@gmail.com>
2015-11-22 22:47:11 +0700

    upload code

---
+++ week 9/test/integration/users_edit_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name,  name
+    assert_equal @user.email, email
+  end
+end
